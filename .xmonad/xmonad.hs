cf425d576b6d2f8f17c3efd82a126c4ba9c8a830
Sahiti <sahiti93@gmail.com>
2016-04-08 19:51:31 +0530

    formatting

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -18,22 +18,20 @@ where fadeAmount = 0.8
 main = do
 xmproc <- spawnPipe "xmobar /home/sahiti/.xmobarcc"
 xmonad $  ewmh
-$  withUrgencyHook NoUrgencyHook defaultConfig { manageHook = manageDocks <+> manageHook defaultConfig
-   		   		 	       , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
-					       , layoutHook =  smartBorders $ avoidStruts $ layoutHook defaultConfig
-					       , logHook = myLogHook <+> dynamicLogWithPP xmobarPP
-					         { ppOutput = hPutStrLn xmproc
-					       	 , ppCurrent = xmobarColor "#b58900" ""
-					       	 , ppTitle = xmobarColor "#b58900" "" . shorten 100
-					       	 , ppUrgent = xmobarColor "yellow" "red" . xmobarStrip 
-					       	 }
-					       , borderWidth		= 0
-					       , modMask			= mod4Mask
-					       , terminal		= "xterm"
-					       , normalBorderColor	= "#002b36"
-					       , focusedBorderColor	= "#586e75"} `additionalKeys`
-				       [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-				       , ((mod4Mask , xK_b), sendMessage ToggleStruts)
-				       , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s ~/Screenshots/%Y-%m-%d-%T-screenshot.png")
-				       , ((0, xK_Print), spawn "scrot")
-				       ]
+       $  withUrgencyHook NoUrgencyHook defaultConfig { manageHook = manageDocks <+> manageHook defaultConfig
+       , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
+       , layoutHook =  smartBorders $ avoidStruts $ layoutHook defaultConfig
+       , logHook = myLogHook <+> dynamicLogWithPP xmobarPP { ppOutput = hPutStrLn xmproc
+       	 	   	     	 		           , ppCurrent = xmobarColor "#b58900" ""
+       							   , ppTitle = xmobarColor "#b58900" "" . shorten 100
+       							   , ppUrgent = xmobarColor "yellow" "red" . xmobarStrip 
+       							   }      
+       , borderWidth		= 0
+       , modMask			= mod4Mask
+       , terminal		= "xterm"
+       , normalBorderColor	= "#002b36"
+       , focusedBorderColor	= "#586e75"} `additionalKeys` [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+       	 			  	     		      , ((mod4Mask , xK_b), sendMessage ToggleStruts)
+							      , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s ~/Screenshots/%Y-%m-%d-%T-screenshot.png")
+							      , ((0, xK_Print), spawn "scrot")
+							      ]
