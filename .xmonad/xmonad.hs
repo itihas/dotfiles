e8cdfe68410016fc1aa59a29a85658cac68b80d8
Sahiti <sahiti93@gmail.com>
2016-05-08 15:48:02 +0530

    .xmonad/xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -64,7 +64,7 @@ main = do
        , normalBorderColor	= "#002b36"
        , focusedBorderColor	= "#586e75"} `additionalKeys` [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
        	 			  	     		      , ((mod4Mask , xK_b), sendMessage ToggleStruts)
-                                                              , ((xK_F5, xK_F5), spawn "exec ~/emacs_capture -e \'(org-capture nil \"i\")\'")
+                                                              , ((0, xK_F5, xK_F5), spawn "exec ~/emacs_capture -e \'(org-capture nil \"i\")\'")
                                                               , ((mod4Mask .|. shiftMask, xK_F5), spawn "exec ~/emacs_capture -e \'(org-capture nil nil)\'")
 							      , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s ~/Screenshots/%Y-%m-%d-%T-screenshot.png")
 							      , ((0, xK_Print), spawn "scrot")
