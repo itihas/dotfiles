0c38562a0a270afe911cf49e4f723324479b2b74
Sahiti <sahiti93@gmail.com>
2016-05-08 13:45:20 +0530

    .xmonad/xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -31,7 +31,7 @@ myLogHook = fadeInactiveLogHook fadeAmount
 myManageHook :: ManageHook
 myManageHook = composeAll . concat $
    [ [ className =? "Firefox-bin" --> doShift   "web" ]
-   , [ className =? "emacs"       --> doShift "emacs" ]
+   , [ className =? "Emacs"       --> doShift "emacs" ]
    , [(className =? "Firefox" <&&> resource =? "Dialog") --> doFloat]
  
      -- using list comprehensions and partial matches
