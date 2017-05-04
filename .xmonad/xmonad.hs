6430a07278aebaf2fc00aa30853b70a562b1f6d0
Sahiti <sahiti93@gmail.com>
2017-05-04 14:38:45 +0530

    .xmonad/xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -62,7 +62,7 @@ main = do
        , normalBorderColor	= "#002b36"
        , focusedBorderColor	= "#586e75"} `additionalKeys` [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
        	 			  	     		      , ((mod4Mask , xK_f), spawn "firefox")
-       	 			  	     		      , ((mod4Mask , xK_g), spawn "exec ~/dmenu_search.sh")
+       	 			  	     		      , ((mod4Mask , xK_s), spawn "exec ~/dmenu_search.sh")
                                                               , ((mod4Mask,  xK_F5), spawn "exec ~/emacs_capture -e \'(org-capture nil \"i\")\'")
 							      , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s ~/Screenshots/%Y-%m-%d-%T-screenshot.png")
 							      , ((0, xK_Print), spawn "scrot")
